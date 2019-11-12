0485177e76223a849b6b44f7ef9170f6eebb2715
Ofraz <ofrazfull@gmail.com>
2019-11-11 22:49:39 -0600

    Corregido error que impedía eliminar Intermediarios utilizados en FK

--- admin/inter/int_delete.php
+++ admin/inter/int_delete.php
@@ -3,8 +3,10 @@
 
     if(isset($_POST['id'])) {
         $id = $_POST['id'];
+        $change = "UPDATE activid SET id_inter = '0' WHERE id_inter = '$id'";
+
         $query = "DELETE FROM inter WHERE id_inter = '$id'";
-        $result = mysqli_query($con, $query);
+        $result = mysqli_query($con,$change) && mysqli_query($con,$query);
         if(!$result) {
             die('Error de Consulta'.mysqli_error($con));
         }
