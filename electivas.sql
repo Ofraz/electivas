2131e465f7d0c5cd0f95c69ede0841e1e50bee12
Ofraz <ofrazfull@gmail.com>
2019-11-20 01:44:09 -0600

    Update SQL

--- electivas.sql
+++ electivas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-11-2019 a las 06:01:11
+-- Tiempo de generación: 20-11-2019 a las 08:41:54
 -- Versión del servidor: 10.4.6-MariaDB
 -- Versión de PHP: 7.1.32
 
@@ -42,10 +42,12 @@ CREATE TABLE `activid` (
 --
 
 INSERT INTO `activid` (`id_act`, `name_act`, `description`, `cupo`, `cred_act`, `id_inter`) VALUES
-(1, 'TEST', 'PRUEBA DE LLAVES FORANEAS', 50, 2, 'UPIICSA'),
-(2, 'probando', 'el codigo ya jala :3', 7, 12, '1234'),
-(3, 'querer', 'dormir', 7, 2, 'UPIICSA'),
-(4, 'como', 'estas', 5, 6, '1234');
+(1, 'TEST', 'PRUEBA DE LLAVES FORANEAS', 50, 3, 'UPIICSA'),
+(2, 'probando', 'el codigo ya jala :3', 7, 12, '123'),
+(3, 'querer', 'dormir', 7, 2, '1234'),
+(4, 'como', 'estas', 5, 6, '0'),
+(36, 'modal', 'registra  de manera correcta', 5, 8, '123'),
+(38, 'dfgbhn', 'xdcfvgbhkjmk', 22, 10, '1234');
 
 -- --------------------------------------------------------
 
@@ -65,11 +67,14 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id_admin`, `password`, `name`, `ap`) VALUES
+('123456789', '12345As!', 'user', 'user'),
 ('2011130598', 'pass', 'Ricardo', 'Jimenez Chavez'),
 ('201130598', 'pass', 'Ricardo', 'Jimenez Chavez'),
 ('admin', 'pass', 'prueba', 'p'),
 ('aide', 'leches', 'aide', 'cruz'),
 ('erika', 'flaca', 'aidecita', 'mil cruz'),
+('JICR', '12345As!', 'tyuty', 'tyuio'),
+('passwd', '1111', 'confirm ', 'pass'),
 ('turrom', 'romi', 'bebe', 'baby'),
 ('validar', 'xc', 'admin', 'pass');
 
@@ -81,7 +86,7 @@ INSERT INTO `admin` (`id_admin`, `password`, `name`, `ap`) VALUES
 
 CREATE TABLE `alu` (
   `boleta` int(10) NOT NULL,
-  `password` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
   `name_a` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `ap_a` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `cred` int(2) NOT NULL
@@ -92,11 +97,8 @@ CREATE TABLE `alu` (
 --
 
 INSERT INTO `alu` (`boleta`, `password`, `name_a`, `ap_a`, `cred`) VALUES
-(2, '', 'sdghj', 'sdghjkl', 0),
-(4, '4', 'cuatro', 'cuatro', 0),
-(1558, 'pass', 'w', 'd', 0),
-(20000, 'pass', 'aidecita', 'mil cruz', 0),
-(201213, '', 'erika', 'cruz', 7),
+(20000, 'pass', 'Aidecita', 'Mil Cruz', 1),
+(201213, '12345As!', 'Erika', 'MILLL', 7),
 (2016600853, '22', 'Alejandra', 'Jimenez Chavez', 4);
 
 -- --------------------------------------------------------
@@ -108,21 +110,19 @@ INSERT INTO `alu` (`boleta`, `password`, `name_a`, `ap_a`, `cred`) VALUES
 CREATE TABLE `alu_act` (
   `id_bol_act` int(11) NOT NULL,
   `boleta` int(10) NOT NULL,
-  `id_act` int(11) NOT NULL
+  `id_act` int(11) NOT NULL,
+  `asist` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `alu_act`
 --
 
-INSERT INTO `alu_act` (`id_bol_act`, `boleta`, `id_act`) VALUES
-(101, 2016600853, 1),
-(102, 2, 1),
-(103, 4, 1),
-(104, 20000, 3),
-(110, 2, 4),
-(111, 4, 4),
-(112, 2016600853, 4);
+INSERT INTO `alu_act` (`id_bol_act`, `boleta`, `id_act`, `asist`) VALUES
+(300, 20000, 3, 1),
+(301, 201213, 3, 0),
+(302, 2016600853, 1, 0),
+(303, 2016600853, 3, 0);
 
 -- --------------------------------------------------------
 
@@ -132,7 +132,7 @@ INSERT INTO `alu_act` (`id_bol_act`, `boleta`, `id_act`) VALUES
 
 CREATE TABLE `inter` (
   `id_inter` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
   `name_inter` varchar(55) COLLATE utf8_unicode_ci NOT NULL,
   `ap_inter` varchar(55) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -143,7 +143,7 @@ CREATE TABLE `inter` (
 
 INSERT INTO `inter` (`id_inter`, `password`, `name_inter`, `ap_inter`) VALUES
 ('0', '', 'SIN', 'ASIGNAR'),
-('123', '', 'aide', 'mil cruz'),
+('123', '12345As!', 'uno', 'dostres'),
 ('1234', '2903', 'test', 'prueba'),
 ('UPIICSA', '', 'escuela', 'ipn');
 
@@ -192,7 +192,13 @@ ALTER TABLE `inter`
 -- AUTO_INCREMENT de la tabla `activid`
 --
 ALTER TABLE `activid`
-  MODIFY `id_act` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_act` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT de la tabla `alu_act`
+--
+ALTER TABLE `alu_act`
+  MODIFY `id_bol_act` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=308;
 
 --
 -- Restricciones para tablas volcadas
@@ -202,14 +208,14 @@ ALTER TABLE `activid`
 -- Filtros para la tabla `activid`
 --
 ALTER TABLE `activid`
-  ADD CONSTRAINT `activid_ibfk_1` FOREIGN KEY (`id_inter`) REFERENCES `inter` (`id_inter`);
+  ADD CONSTRAINT `activid_ibfk_1` FOREIGN KEY (`id_inter`) REFERENCES `inter` (`id_inter`) ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `alu_act`
 --
 ALTER TABLE `alu_act`
-  ADD CONSTRAINT `alu_act_ibfk_1` FOREIGN KEY (`boleta`) REFERENCES `alu` (`boleta`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `alu_act_ibfk_2` FOREIGN KEY (`id_act`) REFERENCES `activid` (`id_act`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `alu_act_ibfk_3` FOREIGN KEY (`boleta`) REFERENCES `alu` (`boleta`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `alu_act_ibfk_4` FOREIGN KEY (`id_act`) REFERENCES `activid` (`id_act`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
